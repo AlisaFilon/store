caaa50b2065f04cb05a54d91f4ee45273ed66b14
Alisa <filonenkoalisa@gmail.com>
2021-05-17 11:45:49 +0200

    crear controller

---
+++ src/main/java/group/store/controllers/ProductController.java
@@ -0,0 +1,14 @@
+package group.store.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class ProductController {
+    @GetMapping("/health")
+    public String check() {
+        return "Hello! I'm a server.";
+    }
+}
