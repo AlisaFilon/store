4b1a29fca860d188abf0ed6885541053e4159071
Alisa <filonenkoalisa@gmail.com>
2021-05-18 12:15:03 +0200

    post mapping

--- src/main/java/group/store/controllers/ProductController.java
+++ src/main/java/group/store/controllers/ProductController.java
@@ -2,6 +2,7 @@ package group.store.controllers;
 
 import group.store.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -22,11 +23,18 @@ public class ProductController {
     @GetMapping("/products")
     public List<Product> allProducts() {
         return products;
+    }
+
+        @PostMapping("/products")
+        public void addProduct(Product product) {
+            products.add(product);
+        }
+
+
+}
 
    /* @DeleteMapping("/product/{name}")
     public String removeProduct(@PathVariable String name)
         return"deleted!";*/
-    }
 
-}
 
